b0efd3911d51d9bb962419f8592a1963fca07485
Lê Văn Phi Hào <72533394+phihao181102@users.noreply.github.com>
2020-12-10 08:28:06 +0700

    Add files via upload

---
+++ Bài tập 3/amduong.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+  int a = 5;
+  // kiem tra a la duong hay am
+  if(a >= 0){
+    printf("%d la so khong am",a);
+
+  } else {
+    printf("%d la so am",a);
+
+  }
+  
+  getchar();
+  
+  return 1;
+
