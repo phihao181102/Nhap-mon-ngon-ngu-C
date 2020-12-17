337860e278463e9f0cdb365b2a8c70563b04b297
Lê Văn Phi Hào <72533394+phihao181102@users.noreply.github.com>
2020-12-17 08:21:12 +0700

    Update hoadon.c

--- Bài tập 3/hoadon.c
+++ Bài tập 3/hoadon.c
@@ -1,14 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main(void){
-  int a, b, c;
-  //khai bao gia tri//
-  printf("nhap so luong");
+  //khai bao gia tri so luong va don gia//
+  int a, b;
+  //tiep theo la nhap khoi luong//
+  printf("nhap so luong vao day");
   scanf("%d",&a);
-  printf("nhap don gia");
+  //va nhap don gia//
+  printf("vui long nhap don gia");
   scanf("%d",&b);
+  //khai boa bien de tinh thanh tien//
   int khoiluong, giatri, thanhtien;
+  //lam theo cong thuc so luong * don gia = thanh tien//
   thanhtien = a * b;
-  printf("a * b = %d",thanhtien);
+  if(a>=5){thanhtien=(a*b)*0.25;
+  }
+  //ta co ket qua dat duoc//
+  printf("thanh tien = %d",thanhtien);
   return 0;
 
 
