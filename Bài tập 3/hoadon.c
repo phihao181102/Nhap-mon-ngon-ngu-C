fafc5b1b5e1842f480039125e411327946b46c61
Lê Văn Phi Hào <72533394+phihao181102@users.noreply.github.com>
2020-12-17 07:45:39 +0700

    Add files via upload

---
+++ Bài tập 3/hoadon.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void){
+  int a, b, c;
+  //khai bao gia tri//
+  printf("nhap so luong");
+  scanf("%d",&a);
+  printf("nhap don gia");
+  scanf("%d",&b);
+  int khoiluong, giatri, thanhtien;
+  thanhtien = a * b;
+  printf("a * b = %d",thanhtien);
+  return 0;
+
+
+}
