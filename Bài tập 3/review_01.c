2ec0d6e5ff6697ee269a63b4d912be44954ebc56
Lê Văn Phi Hào <72533394+phihao181102@users.noreply.github.com>
2020-12-31 08:38:13 +0700

    Add files via upload

---
+++ Bài tập 3/review_01.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main(void) {
+  int n;
+  printf ("Nhap n = "),scanf ("%d",&n);
+  if (n==1)
+  {
+    printf("one");
+  }
+  if (n==2)
+  {
+    printf("Two");
+  }
+  if (n==3)
+  {
+    printf("Three");
+  }
+  if (n==4)
+  {
+    printf("Four");
+  }
+  if (n==5)
+  {
+    printf("Five");
+  }
+  if (n==6)
+  {
+    printf("Six");
+  }
+  if (n==7)
+  {
+    printf("Seven");
+  }
+  if (n==8)
+  {
+    printf("Eight");
+  }
+  if(n==9)
+  {
+    printf("Nine");
+  }
+  else if(n > 9)
+  {
+    printf("Greater than 9");
+  }
+  return 0;
+  
+}
