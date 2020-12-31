edea84101c48e59cec1fe578c5af9cd2af6d35ab
Lê Văn Phi Hào <72533394+phihao181102@users.noreply.github.com>
2020-12-31 10:57:25 +0700

    Add files via upload

---
+++ Bài tập 3/review03.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void) {
+  int a;
+  for(a = 1; a < 21; a= a + 2){
+    if(a != 5){
+      if (a != 11){
+        if(a !=15){
+       printf("%d \n",a);
+        }
+      }
+    }
+  }
+  return 0;
+}
