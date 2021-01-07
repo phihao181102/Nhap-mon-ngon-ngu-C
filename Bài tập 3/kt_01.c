a753a80f76c8cfbf02d737eff68df843b116fed9
Lê Văn Phi Hào <72533394+phihao181102@users.noreply.github.com>
2021-01-07 11:17:02 +0700

    Update kt_01.c

--- Bài tập 3/kt_01.c
+++ Bài tập 3/kt_01.c
@@ -1,10 +1,18 @@
-#include <stdio.h>
- 
-int main(){
-    for(int i = 1; i <= 10;i++){
-        printf("\nBANG NHAN %d\n", i);
-        for(int j = 0; j < 10; j++){
-            printf("\t%d x %d = %d\n", i, j, i*j);
-        }
-    }
-}
+#include<stdio.h>
+int main(void){
+  int a,b,c;
+  a = 2, b = 3, c = 4;
+  printf("\nnbang cuu chuong\n");
+  //bang nhan 2//
+  printf("\tbang nhan 2\n");
+  for(int i= 1;i <11; i++)
+  printf("\t%d x %d = %d\n",a ,i ,a * i);
+  //bang nhan 3//
+  printf("\tbang nhan 3\n");
+  for(int i= 1;i <11; i++)
+  printf("\t%d x %d = %d\n",b ,i ,b * i);
+   //bang nhan 4//
+   for(int i= 1;i <11; i++)
+   printf("\t%d x %d = %d\n",c ,i ,c * i);
+   return 0;
+   }
