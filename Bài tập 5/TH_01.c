55b05fa8f218496aac3f91186b0e286f04d0dc0d
Lê Văn Phi Hào <72533394+phihao181102@users.noreply.github.com>
2021-01-28 14:56:56 +0700

    Update TH_01.c

--- Bài tập 5/TH_01.c
+++ Bài tập 5/TH_01.c
@@ -2,12 +2,11 @@
 #include <math.h>
 
 int main() {
-    // khai báo bi?n
+    // khai bÃ¡o bi?n
 	float a, c, b, delta, x1, x2, x;
 	printf("Nhap vao cac he so phuong trinh bac hai: ");
 	scanf("%f%f%f", &a, &b, &c);
-	// xét duy?t các kh? nang
-	if(a == 0) { // PT b?c 1
+	if(a == 0) { // PT bac 1
 		if(b == 0 && c != 0) {
 			printf("Phuong trinh vo nghiem\n");
 		} else if(b == 0 && c == 0) {
@@ -16,18 +15,18 @@ int main() {
 			x = -c / b;
 			printf("Nghiem phuong trinh = %0.2f", x);
 		}
-	} else { // PT b?c 2
-		delta = b * b - a * c * 4; // tính delta
-		if(delta < 0) { // TH1
+	} else { // PT bac 2
+		delta = b * b - a * c * 4; // tinh delta
+		if(delta < 0) { 
 			printf("PT vo nghiem");
-		} else if(delta == 0) { // TH2
+		} else if(delta == 0) { 
 			x1 = -b/(2 * a);
-			// k?t qu?
+			
 			printf("PT co nghiem kep x = %0.2f", x1);
 		} else { // TH3
 			x1 = (-b + sqrt(delta)) / (2 * a);
 			x2 = (-b - sqrt(delta)) / (2 * a);
-			// k?t qu?
+			// ket qua
 			printf("Nghiem PT la: \nx1 = %0.2f\nx2 = %0.2f", x1, x2);
 		}
 	}
