f2e739e131e9895caaf1c4bbfcde6debbd7197bd
Lê Văn Phi Hào <72533394+phihao181102@users.noreply.github.com>
2021-01-28 10:54:35 +0700

    Add files via upload

---
+++ Bài tập 5/ham_03.c
@@ -0,0 +1,35 @@
+//****************************************CODE**********
+//Khoi khai bao
+#include<stdio.h>
+void Nhap_pt_mang(int v[]);
+void Hien_thi_pt_mang(int v[]);
+void Hien_thi_tong_chan(int v[]);
+void Dem_chan_le_0(int v[]);
+ //khoi ham main
+int main(){
+  int n;
+  printf("Nhap n = ");
+  scanf("%d", &n);
+  int a[n];
+  Nhap_pt_mang(a);
+  Hien_thi_pt_mang(a);
+  Hien_thi_tong_chan(a);
+  Dem_chan_le_0(a);
+  return 0;
+}
+void Nhap_pt_mang(int v[]){
+
+}
+void Hien_thi_pt_mang(int v[]){
+
+}
+void Hien_thi_tong_chan(int v[]){
+  
+}
+void Dem_chan_le_0(int v[]){
+
+}
+
+// khoi dinh nghia ham
+
+//*************************END CODE*****************************
